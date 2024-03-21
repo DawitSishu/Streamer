9920d39d55798a52dfda7440406fd4677ae537b8
bubu <bubu@bubu.bubu>
2024-03-21 11:26:28 +0300

    sth

---
+++ lib/live.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RoomLive extends StatefulWidget {
+  const RoomLive({super.key});
+
+  @override
+  State<RoomLive> createState() => _RoomLiveState();
+}
+
+class _RoomLiveState extends State<RoomLive> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
