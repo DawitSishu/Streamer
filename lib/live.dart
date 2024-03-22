cd35aede9e6dffe0f7815e2fc3ad61e0ab166ead
bubu <bubu@bubu.bubu>
2024-03-22 16:13:28 +0300

    setup

--- lib/live.dart
+++ lib/live.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class RoomLive extends StatefulWidget {
-  const RoomLive({super.key});
+  const RoomLive({Key? key}) : super(key: key);
 
   @override
   State<RoomLive> createState() => _RoomLiveState();
@@ -10,6 +10,81 @@ class RoomLive extends StatefulWidget {
 class _RoomLiveState extends State<RoomLive> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "title",
+          style: TextStyle(color: Colors.white),
+        ),
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.grey.shade800, Colors.grey.shade600],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+        ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.grey.shade800, Colors.grey.shade600],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Center(
+          child: Text(
+            'text',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.grey.shade700,
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          height: kToolbarHeight,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Type a message...',
+                    hintStyle: TextStyle(color: Colors.white70),
+                    border: InputBorder.none,
+                  ),
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              IconButton(
+                onPressed: () {
+                  // Add send button functionality
+                },
+                icon: Icon(
+                  Icons.send,
+                  color: Colors.white,
+                ),
+              ),
+              IconButton(
+                onPressed: () {
+                  // Add love icon button functionality
+                },
+                icon: Icon(
+                  Icons.favorite,
+                  color: Colors.red,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
